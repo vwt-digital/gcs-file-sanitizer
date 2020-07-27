4ca954b6d9ecf591f88c8f7f5c9bf1e11d51e433
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2020-07-27 14:18:45 +0200

    Added entity processing for file sanitizing. DAT-3882

--- functions/gcs-file-sanitizer/README.md
+++ functions/gcs-file-sanitizer/README.md
@@ -28,6 +28,20 @@ the sanitizing is done in chunks of max 10MB.
 
 > E.g. The standard maximum file size will accept files not bigger than `256MB`, therefore the function must be set at a minimal memory of `1024MB` 
 
+### Status processing
+When using this function it is possible to report when a file failed to process with declaring two environment 
+variables: `FILE_DATABASE_TYPE` and `FILE_DATABASE_NAME` (see [Deployment to GCP](#deployment-to-gcp)). With these 
+variables a file entity will be retrieved and updated with a status change to `failed`. This can help you to follow 
+up on files that aren't sanitized.
+
+The following fields will be updated within the entity:
+- `status` `[string]`: The status of the current file, defaults to `failed`;
+- `updated_at` `[string]`: The current timestamp in format `2020-01-01T12:00:00.000Z`.
+
+This function supports two GCP databases and has to be set by the environment variable `FILE_DATABASE_TYPE`:
+- `datastore`: [Google Cloud Datastore](https://cloud.google.com/datastore/docs)
+- `firestore`: [Google Cloud Firestore](https://cloud.google.com/firestore/docs)  
+
 ### Deployment to GCP
 An example of a Google Cloud Build step is defined within the [cloudbuild.example.yaml](functions/gcs-file-sanitizer/cloudbuild.example.yaml).
 Within this example, the variables below need to be updated to your needs:
@@ -35,6 +49,8 @@ Within this example, the variables below need to be updated to your needs:
 - `--memory` `[string]`: The [Cloud Function memory](https://cloud.google.com/sdk/gcloud/reference/functions/deploy#--memory) memory size.
 - `TARGET-BUCKET-NAME` `[string]`: The GCS Bucket name the sanitized files will be moved to;
 - `MAX_FILE_SIZE` `[integer]`: The maximum file size to be processed in bytes, defaults to `268435456 bytes / 256MB`;
+- `FILE_DATABASE_TYPE` `[string]`: The GCP Database type (see [Status processing](#status-processing));
+- `FILE_DATABASE_NAME` `[string]`: The GCP Database Table name (see [Status processing](#status-processing)).
 
 ### License
 This function is licensed under the [GPL-3](https://www.gnu.org/licenses/gpl-3.0.en.html) License
--- functions/gcs-file-sanitizer/main.py
+++ functions/gcs-file-sanitizer/main.py
@@ -5,18 +5,57 @@ import logging
 import json
 import math
 import tempfile
+import datetime
 
 import google.auth
 import google.auth.transport.requests as tr_requests
 import numpy as np
 
-from google.cloud import storage
+from google.cloud import storage, datastore, firestore
 from google.resumable_media.requests import ChunkedDownload, ResumableUpload
 
 from PIL import Image
 from PyPDF2 import PdfFileReader, PdfFileWriter, utils
 
 logging.getLogger().setLevel(logging.INFO)
+
+
+class DBDatastore(object):
+    def __init__(self, db_name):
+        self.db_client = datastore.Client()
+        self.db_name = db_name
+
+    def update_status(self, object_id, status):
+        entity_key = self.db_client.key(self.db_name, object_id)
+        entity = self.db_client.get(entity_key)
+
+        if not entity:
+            return False
+
+        entity['status'] = status
+        entity['updated_at'] = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+        self.db_client.put(entity)
+        return True
+
+
+class DBFirestore(object):
+    def __init__(self, db_name):
+        self.db_client = firestore.Client()
+        self.db_name = db_name
+
+    def update_status(self, object_id, status):
+        doc_ref = self.db_client.collection(self.db_name).document(object_id)
+        doc = doc_ref.get()
+
+        if not doc.exists:
+            return False
+
+        doc['status'] = status
+        doc['updated_at'] = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+        doc_ref.update(doc)
+        return True
 
 
 class GGSFileSanitizer(object):
@@ -36,21 +75,28 @@ class GGSFileSanitizer(object):
             logging.info(
                 f"File '{data['name']}' too big to process (size: {data['size']}, max: {self.max_file_size}), " +
                 "skipping sanitizing")
-            sys.exit(0)
+            process_status('failed', data['name'])
+            return
 
         # Only sanitizing files of certain type
         if data['contentType'] not in ['application/pdf', 'image/png', 'image/jpeg']:
             logging.info(
                 f"File '{data['name']}' not of type 'application/pdf', 'image/png' or 'image/jpeg', " +
                 "skipping sanitizing")
-            sys.exit(0)
+            process_status('failed', data['name'])
+            return
 
         logging.info(f"Starting sanitizing of file '{data['name']}'")
 
         if data['contentType'] in ['image/jpeg', 'image/png']:
-            self.process_image(data)
+            is_processed = self.process_image(data)
         else:
-            self.process_pdf(data)
+            is_processed = self.process_pdf(data)
+
+        if is_processed:
+            logging.info(f"Finished sanitizing of file '{data['name']}' successfully")
+        else:
+            process_status('failed', data['name'])
 
     def process_image(self, data):
         # Setting chunk sizes
@@ -88,6 +134,9 @@ class GGSFileSanitizer(object):
                 current_chunk += 1
         except Exception as e:
             logging.exception(e)
+            return False
+
+        return True
 
     def process_pdf(self, data):
         # Setting chunk sizes
@@ -112,7 +161,7 @@ class GGSFileSanitizer(object):
         except Exception as e:
             logging.info(
                 f"An exception occurred while downloading file '{data['name']}', skipping sanitizing: {str(e)}")
-            sys.exit(0)
+            return False
 
         writer = PdfFileWriter()
         try:
@@ -127,7 +176,7 @@ class GGSFileSanitizer(object):
         except Exception as e:
             logging.info(
                 f"An exception occurred when reading PDF file '{data['name']}', skipping sanitizing: {str(e)}")
-            sys.exit(0)
+            return False
 
         # Start file upload
         current_chunk = 0
@@ -157,7 +206,32 @@ class GGSFileSanitizer(object):
         except Exception as e:
             logging.info(
                 f"An exception occurred while uploading file '{data['name']}', skipping sanitizing: {str(e)}")
-            sys.exit(0)
+            return False
+
+        return True
+
+
+def process_status(status, entity_id):
+    if os.environ.get('FILE_DATABASE_TYPE') and os.environ.get('FILE_DATABASE_NAME'):
+        db_type = os.environ.get('FILE_DATABASE_TYPE')
+        db_name = os.environ.get('FILE_DATABASE_NAME')
+
+        if db_type == 'datastore':
+            db_class = DBDatastore(db_name)
+        elif db_type == 'firestore':
+            db_class = DBFirestore(db_name)
+        else:
+            logging.error("Function does not have correct configuration to process statuses")
+            return False
+
+        status = db_class.update_status(entity_id, status)
+
+        if status:
+            logging.info(f"Entity for file '{entity_id}' successfully updated in database '{db_type}/{db_name}'")
+        else:
+            logging.info(f"Entity for file '{entity_id}' does not exist in database '{db_type}/{db_name}'")
+    else:
+        logging.debug('Skipping processing of status due to missing configuration')
 
 
 def gcs_file_sanitizer(data, context):
